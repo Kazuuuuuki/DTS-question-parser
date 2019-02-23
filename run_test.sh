79f8b5db496e210a650246aded785c2cff8da1e6
Koji Mineshima <mineshima.koji@ocha.ac.jp>
2019-02-23 17:54:36 +0900

    show each problem in result main file

--- run_test.sh
+++ run_test.sh
@@ -8,7 +8,6 @@ mkdir -p $results_dir
 
 total=0
 correct=0
-time=0
 
 # Compile coqlib.v
 coqc coqlib.v
@@ -29,15 +28,13 @@ for f in ./testset/*.ccg; do
 done
 
 accuracy=`echo "scale=4; $correct / $total" | bc -l`
-average_time=`echo "scale=4; $time / $total" | bc -l`
 echo "Accuracy: "$correct" / "$total" = "$accuracy
-echo "Average time: ${average_time}"
 
 echo "<!doctype html>
 <html lang='en'>
 <head>
   <meta charset='UTF-8'>
-  <title>Evaluation results of FraCaS</title>
+  <title>Evaluation results</title>
   <style>
     body {
       font-size: 1.5em;
@@ -47,9 +44,9 @@ echo "<!doctype html>
 <body>
 <table border='1'>
 <li>Accuracy : "$correct" / "$total" = "$accuracy"</li>
-<li>Average time: "${average_time}"</li>
 <tr>
-  <td>FraCaS problem</td>
+  <td>id</td>
+  <td>problem</td>
   <td>gold answer</td>
   <td>system answer</td>
 </tr>" > $results_dir/main.html
@@ -61,11 +58,12 @@ gray_color="rgb(136,136,136)"
 
 for gold_filename in `ls -v ./testset/*.answer`; do
   fname=${gold_filename##*/}
+  sentences=`python get_sentences.py ./testset/${fname/.answer/.ccg} | sed 's|#END#|<br>|g'`
   system_answer=`cat $results_dir/${fname/.ccg/.answer} | awk -F',' '{print $1}'`
-  time=`cat $results_dir/${fname/.ccg/.answer} | awk -F',' '{print $2}'`
   gold_answer=`cat $gold_filename`
   echo '<tr>
   <td>'${fname/.answer/}'</td>
+  <td>'$sentences'</td>
   <td>'${gold_answer}'</td>' >> $results_dir/main.html
   color=$white_color
   if [ "$gold_answer" == "yes" ] || [ "$gold_answer" == "no" ]; then
